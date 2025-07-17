3a553f27b7b50044ba0aab2d80b3acb263db4ab6
jaesik <ojs72272@gmail.com>
2025-07-17 16:43:50 +0900

    feat: dockerfile

--- models/userprofile.js
+++ models/userprofile.js
@@ -2,8 +2,8 @@ module.exports = (sequelize, DataTypes) => {
   const UserProfile = sequelize.define('UserProfile', {
     user_id: { type: DataTypes.INTEGER, primaryKey: true },
     nickname: { type: DataTypes.STRING(50), unique: true, allowNull: true },
-    profile_icon_id: { type: DataTypes.INTEGER, defaultValue: 0 },
-    profile_char_id: { type: DataTypes.INTEGER, defaultValue: 0 },
+    profile_icon_id: { type: DataTypes.INTEGER, defaultValue: 1 },
+    profile_char_id: { type: DataTypes.INTEGER, defaultValue: 1 },
     level: { type: DataTypes.INTEGER, defaultValue: 1 },
     exp: { type: DataTypes.INTEGER, defaultValue: 0 },
     gold: { type: DataTypes.INTEGER, defaultValue: 0 },
